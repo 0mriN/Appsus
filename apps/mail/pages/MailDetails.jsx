e89b59c45355fdef75818ba6bff22d64a33cc863
Omri Nadav <omrinadav15@gmail.com>
2024-06-26 22:49:54 +0300

    Add details

---
+++ apps/mail/pages/MailDetails.jsx
@@ -0,0 +1,15 @@
+// import { mailService } from "../services/mail.service.js"
+
+// const { useEffect } = React
+
+// export function MailDetails({ mailId }) {
+
+//     useEffect(()=>{
+//         mailService.get(mailId).then(mail=> console.log('mail:', mail))
+//     })
+
+
+//     return (
+//         <h1>hello</h1>
+//     )
+// }
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,27 +1,56 @@
 import { MailList } from "../cmps/MailList.jsx";
 import { mailService } from "../services/mail.service.js";
-
+import { MailDetails } from "./MailDetails.jsx";
 
 const { useEffect, useState } = React
 
 export function MailIndex() {
 
     const [mails, setMails] = useState(null)
+    const [filterBy, setFilterBy] = useState(mailService.getEmptyMail())
+    const [selectedMailId, setSelectedMailId] = useState(null)
+
+
 
     useEffect(() => {
-        mailService.query()
-            .then(mails => setMails(mails))
+        loadMails()
+    }, [filterBy])
+
+    function loadMails() {
+        mailService.query(filterBy)
+            .then(mails => {
+                setMails(mails.sort((a, b) => b.sentAt - a.sentAt))
+            })
             .catch(err => {
                 console.log('err:', err)
-            },[])
-    })
-if(!mails) return <div>Loading...</div>
+            })
+    }
+
+    function onRemoveMail(mailId) {
+        mailService.remove(mailId)
+            .then(() => setMails(prevMails => mails.filter(mail => mail.id !== mailId)))
+            .catch(err => {
+                console.log(`oops! looks like something went wrong in removing this book ${bookId}:`, err);
+            })
+    }
+
+function onSelectMailId(mail){
+    setSelectedMailId(mail)
+}
+
+    if (!mails) return <div>Loading...</div>
     return (
         <section className="mail-index">
-        <div>mail app</div>
-            <MailList mails={mails} />
-
-
+            {!selectedMailId ?
+                <React.Fragment>
+                    <MailList
+                     mails={mails}
+                     onRemoveMail={onRemoveMail}
+                     onSelectMailId={onSelectMailId} 
+                     />
+                </React.Fragment>
+                : <MailDetails />
+            }
         </section>
     )
 }
